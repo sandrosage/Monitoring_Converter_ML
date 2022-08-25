d0b670c727c63a63a632fc2028f045d68088e391
sandrosage <sandro-sage@gmx.de>
2022-08-25 15:36:37 +0200

    add more comments ML part

--- search_errors.py
+++ search_errors.py
@@ -1,10 +1,15 @@
+##############################################################################
+# this code is only for checking wether the specific error still occurs or not
+##############################################################################
+
 import pandas as pd
 
 arr = ["drossel", "normal", "mosfets"]
 naming = ["DROSSEL", "NORMAL", "MOSFETS"]
-output_file = open("error_occurances.txt", "w")
+output_file = open("error_occurances.txt", "w")     # file as table to show the occurances of error per file
 
 
+# iterate through the directory an its subdirectories and files
 for j in range(0,3):
     
     for i in range(0,6):
@@ -12,14 +17,16 @@ for j in range(0,3):
         print(text)
         file = open(text, "r")
 
-        #read content of file to string
+        # read content of file to string
         data = file.read()
 
-        #get number of occurrences of the substring in the string
+        # get number of occurrences of the substring in the string
         occurrances = data.count("3068.")
 
         statictest = naming[j] + "_" + str(i) + "A" 
         occ = str(occurrances) + "\n"
+
+        # store everything into the mentioned file
         output_file.write(statictest + ": " + occ)
         
         file.close()
